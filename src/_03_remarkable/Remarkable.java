24a353e5dd9701297c7eaf8aa3162f7b7923c7a6
Mclaughlin.Finn <Mclaughlin.Finn@SEMS-259-29.SEMS259.LAB>
2019-11-13 16:17:37 -0800

    pizza

--- src/_03_remarkable/Remarkable.java
+++ src/_03_remarkable/Remarkable.java
@@ -8,10 +8,13 @@ public class Remarkable {
 public static void main(String[] args) {
 
 		// 1. Save something remarkable about each person in a variable.
+	String response=JOptionPane.showInputDialog("What is remarkable about you?");
+
 
 		// 2. Ask the user to enter a name. Store their answer in a variable.
-
+	String response2=JOptionPane.showInputDialog("What is your name?");
 		// 3. In a pop-up, tell the user what is remarkable about that person. 
+	JOptionPane.showMessageDialog(null, "you are "+response+", "+response2+""); 
 
 	}
 }
--- src/_05_tasty_tomato/tasty_tomato/tasty_tomato.pde
+++ src/_05_tasty_tomato/tasty_tomato/tasty_tomato.pde
@@ -4,7 +4,11 @@ void setup() {
 void draw() {
     background(200, 200, 200);
     noStroke();
+    fill(#FF0000);
     ellipse(150, 200, 150, 150);
     ellipse(212, 200, 150, 150);
+    fill(#00FF12);
     rect(176, 103, 12, 32);
+    fill(200);
+    ellipse(250,200,100,100);
 }
