2931466433f361d739e673b3818c5dbed9e65920
McLaughlin.Finn <McLaughlin.Finn@SEMS-259-29.SEMS259.LAB>
2019-12-11 16:25:42 -0800

    ice cream banana

--- src/_09_zombie_eyes/zombie_eyes/zombie_eyes.pde
+++ src/_09_zombie_eyes/zombie_eyes/zombie_eyes.pde
@@ -1,7 +1,13 @@
 
 void setup() {
+size(800,600);
 
 }
 void draw() {
-
+PImage face = loadImage("Cat Eyes.png");
+face.resize(500,500);
+image(face, 100, 100);
+fill(#BC0F1E);
+ellipse(480,350, 50,50);
+ellipse(220,350, 50,50);
 }
---
+++ src/_12_badgers/Badgers.java
@@ -0,0 +1,41 @@
+package _12_badgers;
+
+public class Badgers {
+public static void main(String[] args) {
+	for (int j = 0; j < 4; j++) {
+		
+	
+	for (int i = 0; i < 12; i++) {
+		System.out.println("Badger");
+	}
+	for (int i = 0; i < 2; i++) {
+		System.out.println("Mushroom");
+		
+		
+	}
+	
+	
+	}
+	
+	
+	for (int i = 0; i < 5; i++) {
+			System.out.println("Snake");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
